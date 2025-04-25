9bf19f4d183338f161e0962b916c7d56641bd508
octavio <vizdoagaoctavio33@gmail.com>
2025-04-25 14:53:00 +0300

    Size of skills grow when hover and shrink when not hovering

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+let skills = document.querySelectorAll('.skill');
+
+skills.forEach(skill => {
+  skill.addEventListener('mouseenter', event => {
+    event.target.style.animationName = 'growSize';
+    event.target.style.animationDuration = '0.35s';
+    event.target.style.animationFillMode = 'forwards';
+  });
+});
+
+skills.forEach(skill => {
+  skill.addEventListener('mouseleave', event => {
+    event.target.style.animationName = 'shrinkSize';
+    event.target.style.animationDuration = '0.25s';
+    event.target.style.animationFillMode = 'forwards';
+  });
+});
