5ba3a5c007766407922a8b743671958551a52a27
octavio <vizdoagaoctavio33@gmail.com>
2025-04-25 15:42:31 +0300

    Add interactivity with the project When you click the project, the project info pops up. When you click the project again, the project info disapears.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
-let skills = document.querySelectorAll('.skill');
+const skills = document.querySelectorAll('.skill');
+const projects = document.querySelectorAll('.project');
 
 skills.forEach(skill => {
   skill.addEventListener('mouseenter', event => {
@@ -13,5 +14,27 @@ skills.forEach(skill => {
     event.target.style.animationName = 'shrinkSize';
     event.target.style.animationDuration = '0.25s';
     event.target.style.animationFillMode = 'forwards';
+  });
+});
+
+projects.forEach(project => {
+  let isOpen = false;
+
+  const projectInfo = project.childNodes[3];
+  const header = project.childNodes[1];
+  const headerContent = header.innerText;
+  const arrow = 0;
+  const newHeaderContent = headerContent.slice(0, arrow) + headerContent.slice(arrow + 1);
+
+  project.addEventListener('click', () => {
+    if(isOpen){
+      projectInfo.style.display = 'none';
+      header.innerHTML = `→ ${newHeaderContent}`;
+      isOpen = false;
+    } else {
+      projectInfo.style.display = 'block';
+      header.innerHTML = `↓ ${newHeaderContent}`;
+      isOpen = true;
+    }
   });
 });
